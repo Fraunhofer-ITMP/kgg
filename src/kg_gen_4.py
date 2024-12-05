a848570493564f6ee67b625a94f95ce6012fd994
jrkarki <reagonkarki@gmail.com>
2024-12-05 09:33:01 +0100

    typos fixed

--- src/kg_gen_4.py
+++ src/kg_gen_4.py
@@ -746,7 +746,9 @@ def saveFiles(kgName, disease2protein, drugAdvEffect, final_kg, drug_df,snp_df,u
 
     print('Now let\'s save all the files that were created in the process.','\n')
 
-    print('Please enter the location where KG files should be stored. A folder will be created automatically. For local folders, use this example: C:\\Users\\rkarki\\Documents\\kg\\. For servers like mybinder or EGI notebook, use this example: /home/jovyan/kgg/data/kgs/test/','\n')
+    print('Please enter the location where KG files should be stored. A folder will be created automatically.','\n')
+    print('For local folders, use this example: C:\\Users\\rkarki\\Documents\\kg\\','\n')
+    print('For servers like mybinder or EGI notebook, use this: /home/jovyan/data/kgs/test/','\n')
     
     time.sleep(0.2)
     
